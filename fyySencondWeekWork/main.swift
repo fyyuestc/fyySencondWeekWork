6ba8046bf04fab7677f654acd10aad3411a62dc9
范云翼 <fanyunyimac@fanyunyimacdemac.local>
2018-09-05 15:30:09 +0800

    Initial Commit

---
+++ fyySencondWeekWork/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fyySencondWeekWork
+//
+//  Created by fanyunyimac on 2018/9/5.
+//  Copyright © 2018年 范云翼. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
